d757ece7f83e83b27333f862e37065ab957bde5a
Marto <martindimitrov040823@gmail.com>
2021-10-07 16:16:54 +0300

    fibonacci function added

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,33 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main(void) {
- int n, First=0, Second=1, NextRotation=0;
- cout<<" N=?";
 
-      cin>>n;
+void fibonacci(){
+	int n=0;
+	int First=0;
+	int Second=1;
+	int NextRotation=0;
 
-     for (int i=0; i<=n;i++){
-
-     if (i==0)
-     {
-
-     cout<<First<<", ";
-     continue;
-     }
-
-     if (i==2)
-     {
-     cout<<Second<<", ";
-     continue;
-     }
-     NextRotation=First+Second;
-     First=Second;
-     Second=NextRotation;
-     cout<<NextRotation<<", ";
-
- }
- 
-     return(0);
-
+	cin >> n;
+	for (int i=0; i<=n;i++){
+		NextRotation=First+Second;
+		First=Second;
+		Second=NextRotation;
+	}
+	cout<<NextRotation<<endl;
 }
+int main() {
+	cout<<" N=?";
+	fibonacci();
+	return(0);
+}
+

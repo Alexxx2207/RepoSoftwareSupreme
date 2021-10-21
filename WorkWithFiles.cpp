d788c28bbf83be033472293662b1e2dc48edb3ec
Marto <martindimitrov040823@gmail.com>
2021-10-21 15:04:01 +0300

    it works... well kinda
    Need to check if a line is empty -> create a func to do that

--- WorkWithFiles.cpp
+++ WorkWithFiles.cpp
@@ -7,49 +7,55 @@ int main()
    FILE *file;
 
 	file = fopen("newprogram.txt","r");
-	int FirstNum;
-	int sum=0;
+	
+	
 	if (file == NULL)
 	{
 		printf ("Error!");
 		exit (1);
 	}
 
-	char array[256];
-	fscanf (file,"%d\n",&FirstNum);
-	
-	for (int i=0;i<FirstNum;i++)
-	{
+	char array[256];	
+	do{
+		int avg=0;
+		int sum=0;
+		int FirstInLine=0;
 		fgets (array, 256, file);
+		sscanf(array,"%d\n",&FirstInLine);
+		int check=0;
 		
-			for (int j = 0; j < 256; j++)
-			{
-				
-				
-					const char ch = array[j];
-					int FirstInLine;
-
-					sscanf(array,"%d",&FirstInLine);
-
-                    
-						if (ch == '\0')
-						{
-							printf("\n");
-							break;
-						}
-						else if (ch != ' ' && ch != '\n')
-						{
-							int num = 0;
-							sscanf(&array[j], "%d", &num);
-							//printf("%d %d ", num, j);
-							sum += num;
-						}
-					
+		for (int j = 0; j < 256; j++)
+		{
+			
+			if (check>=FirstInLine)
+			{	
+				break;
 			}
-			printf("%d", sum);
-			sum = 0;
-		       
-	}
+			const char ch = array[j];
+
+			if (ch == '\0')
+			{
+				printf("\n");
+				break;
+			}
+			else if (ch != ' ' && ch != '\n')
+			{
+				if(ch>0 && array[j-1] == ' ')
+				{
+					int num = 0;
+					sscanf(&array[j], "%d", &num);
+					//printf("%d %d ", num, j);
+					sum += num;
+					check++;
+				}
+			}
+				
+		}
+		avg=sum/FirstInLine;
+		printf("%s%d\n","The avg on line is :", avg);
+		
+	}while (feof(file) == 0 );
+	
 	        
         
    fclose(file);

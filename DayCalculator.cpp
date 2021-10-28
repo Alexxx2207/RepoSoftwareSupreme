6a1a6d84c4875e4adf549bfbf9b72062e82eb294
Marto <martindimitrov040823@gmail.com>
2021-10-28 18:11:25 +0300

    Half Done

--- DayCalculator.cpp
+++ DayCalculator.cpp
@@ -1,129 +1,146 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+ void Menu()
+ {
+	printf("Press 1 or 2\n");
+	printf("1. If you want to check day\n");
+	printf("2. If you want to check day + 1\n");
+ }
 int main()
 {
-int day;
-int month;
-int year;
-cout<<"Enter year: ";
-cin>>year;
-if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-{
-	do{
-	cout<<"Enter a valid month: ";
-	cin>>month;
-	}while(month > 12);
-	if (month = 2)
+	Menu();
+	int day;
+	int month;
+	int year;
+	int choise;
+	scanf("%d", &choise);
+	switch (choise)
+	{
+	case 1 : 
+	printf("Enter year: ");
+	scanf("%d", &year);
+	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
 	{
 		do{
-		cout<<"Enter a valid day: ";
-		cin>>day;
-		}while(day > 29);
+			printf("Enter a valid month: ");
+			scanf("%d",&month);
+		}while(month > 12);
+		if (month == 2)
+		{
+			do{
+			printf("Enter a valid day: ");
+			scanf("%d", &day);
+			}while(day > 29);
 
-	}else{
-
+		}else{
 		do{
-		cout<<"Enter a valid day: ";
-		cin>>day;
+			printf("Enter a valid day: ");
+			scanf("%d", &day);
 		}while(day > 31);
 	}
 int M;
-if (month>2)
-{
-	M=month;
-}else 
-{
- M=month+12;
- year--;
-}
-int Y= year % 100;
-int C= year / 100;
-int W= (day + floor((13*(M+1)) / 5) + Y + floor(Y/4) + floor(C/4) + C*5);
-W = W % 7;
-switch (W)
-{
-case 0 :
-	cout<<"Saturday";
-	break;
-case 1 :
-	cout<<"Sunday";
-	break;
-case 2 :
-	cout<<"Monday";
-	break;
-case 3 :
-	cout<<"Tuesday";
-	break;
-case 4 :
-	cout<<"Wednesday";
-	break;
-case 5 :
-	cout<<"Thursday";
-	break;
-case 6 :
-	cout<<"Friday";
-	break;
-}
-}else
-{
+	if (month>2)
+	{
+		M=month;
+	} else
+	{
+ 		M=month+12;
+ 		year--;
+	}
+	int Y= year % 100;
+	int C= year / 100;
+	int W= (day + floor((13*(M+1)) / 5) + Y + floor(Y/4) + floor(C/4) + C*5);
+	W = W % 7;
+	switch (W)
+	{
+	case 0 :
+		printf("Saturday");
+		break;
+	case 1 :
+		printf("Sunday");
+		break;
+	case 2 :
+		printf("Monday");
+		break;
+	case 3 :
+		printf("Tuesday");
+		break;
+	case 4 :
+		printf("Wednesday");
+		break;
+	case 5 :
+		printf("Thursday");
+		break;
+	case 6 :
+		printf("Friday");
+		break;
+	}
+	}else
+	{
 	do{
-	cout<<"Enter month: ";
-	cin>>month;
+		printf("Enter month: ");
+		scanf("%d",&month);
 	}while(month > 12);
-	if (month = 2)
+	if (month == 2)
 	{
 		do{
-		cout<<"Enter a valid day: ";
-		cin>>day;
+			printf("Enter a valid day: ");
+			scanf("%d",&day);
 		}while(day > 28);
 
 	}else{
 
 		do{
-		cout<<"Enter a valid day: ";
-		cin>>day;
+			printf("Enter a valid day: ");
+			scanf("%d",&day);
 		}while(day > 31);
 	}
 
 
-int M;
-if (month>2)
-{
-	M=month;
-}else 
-{
- M=month+12;
- year--;
-}
-int Y= year % 100;
-int C= year / 100;
-int W= (day + floor((13*(M+1)) / 5) + Y + floor(Y/4) + floor(C/4) + C*5);
-W = W % 7;
-switch (W)
-{
-case 0 :
-	cout<<"Saturday";
-	break;
-case 1 :
-	cout<<"Sunday";
-	break;
-case 2 :
-	cout<<"Monday";
-	break;
-case 3 :
-	cout<<"Tuesday";
-	break;
-case 4 :
-	cout<<"Wednesday";
-	break;
-case 5 :
-	cout<<"Thursday";
-	break;
-case 6 :
-	cout<<"Friday";
-	break;
-}
+	int M;
+	if (month>2)
+	{
+		M=month;
+	}else 
+	{
+ 		M=month+12;
+ 		year--;
+	}
+	int Y= year % 100;
+	int C= year / 100;
+	int W= (day + floor((13*(M+1)) / 5) + Y + floor(Y/4) + floor(C/4) + C*5);
+	W = W % 7;
+	switch (W)
+	{
+	case 0 :
+		printf("Saturday");
+		break;
+	case 1 :
+		printf("Sunday");
+		break;
+	case 2 :
+		printf("Monday");
+		break;
+	case 3 :
+		printf("Tuesday");
+		break;
+	case 4 :
+		printf("Wednesday");
+		break;
+	case 5 :
+		printf("Thursday");
+		break;
+	case 6 :
+		printf("Friday");
+		break;
+	}
 
 }
+		break;
+	
+	 
+
+	
+	} 
 }

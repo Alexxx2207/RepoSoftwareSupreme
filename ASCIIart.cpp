3993baf727ca38078fd15f8b85abe2494bab3c7c
Marto <martindimitrov040823@gmail.com>
2021-10-29 17:27:16 +0300

    ASCII part2

--- ASCIIart.cpp
+++ ASCIIart.cpp
@@ -2,27 +2,25 @@
 using namespace std;
 int main()
 {
-	int redove, num = 1;
+	int redove;
 	printf("Enter number of rows: ");
 	scanf("%d", &redove);
 
 	for(int i = 0; i < redove; i++)
 	{
 	for(int spacing = 0; spacing <= redove-i; spacing++ )
-		printf("  ");
+		printf(" ");
 		for(int j = 0; j <= i; j++)
 		{
-			if (j == 0 || i == 0)
-			{
-				num = 1;
-			}else
-			{
-				num = num*(i-j+1)/j;
-			}
-			printf("%3d ", num);
+			printf("#");
+		}
+		printf(" ");
+		for(int j = 0; j <= i; j++)
+		{
+			printf("#");
 		}
 		printf("\n");
 	}
-
+	
 	return 0;
 }

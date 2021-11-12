5589eb1c04d96efc3776e6c54eb7480b61f181d0
Marto <martindimitrov040823@gmail.com>
2021-11-12 14:46:57 +0200

    PlayerVision added to Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
 #include <stdio.h>
-
 int CustomGetLine(FILE* file ,char Array[256][256])
 {
 	char temp[256];
-	for (int i=0; i< 10; i++)
-	{
+	int i = 0;
+	for (; feof(file) == 0; i++)
+	{	
 		fgets(temp, 256, file);
-		for (int j=0;temp[j] != '\0'; j++){
+		for (int j=0;temp[j] != '\0'; j++)
+		{	
 				if(temp[j] == '\n') {
 					temp[j] = '\0';
+					
 				}
 				Array[i][j]=temp[j];
 		}
 	}
+	return i;
 }
-void PrintMap(char Array[256][256])
+// int CharsOnRow(FILE* file, char Array[256][256])
+// {
+// 	int charsonrow = 0;
+// 	char temp[256];
+// 	fgets(temp, 256, file);
+// 		for (int j=0;temp[j] != '\0'; j++)
+// 		{	
+// 				charsonrow++;
+// 		}
+// 		return charsonrow;
+// }
+//}
+void PlayerVision(int &x, int &y, char Array[256][256],int player)
 {
-		for(int j = 0;j < 10;j++)
+	for(int i = -2; i < 3; i ++)
+	{
+		for (int j = -2; j < 3; j++)
 		{
-			printf("%s\n\r", &Array[j]);
+			printf("%c",Array[y+i][x+j]);
+			
 		}
+		printf("\n\r");
+	}
 }
 int main(){
 
@@ -33,11 +53,13 @@ int main(){
 		exit (1);
 	}
 	char Array[256][256];
+	
 	const char player = '@';
 	int y = 1;
 	int x = 1;
 	char movechar;
-	CustomGetLine(file,Array);
+	// int charsonrow = CharsOnRow(file,Array);
+	int rows = CustomGetLine(file,Array);
 	do{
 		system ("/bin/stty raw");
 		movechar = getchar();
@@ -74,7 +96,8 @@ int main(){
 		}
 		system("clear");
 		Array[y][x] = player;
-		PrintMap(Array);
+		PlayerVision(x,y,Array,player);
+		//PrintMap(Array,rows);
 		Array[y][x] = ' ';
 		
 	}while( movechar != 'q' && movechar != 'Q');
